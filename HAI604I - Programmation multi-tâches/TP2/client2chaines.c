83b5bdc33a20a02874f638121de38f5d7d8aa45e
thibaud.paulin01@etu.umontpellier.fr <thibaud.paulin@gmail.com>
2025-01-27 12:32:56 +0100

    les tests la en c

---
+++ HAI604I - Programmation multi-tâches/TP2/client2chaines.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <string.h>
+
+/* Programme client */
+
+int main(int argc, char *argv[])
+{
+
+   /* je passe en paramètre l'adresse de la socket du serveur (IP et
+      numéro de port) et un numéro de port à donner à la socket créée plus loin.*/
+
+   /* Je teste le passage de parametres. Le nombre et la nature des
+      paramètres sont à adapter en fonction des besoins. Sans ces
+      paramètres, l'exécution doit être arrétée, autrement, elle
+      aboutira à des erreurs.*/
+   if (argc != 4)
+   {
+      printf("utilisation : %s ip_serveur port_serveur port_client\n", argv[0]);
+      exit(1);
+   }
+
+   /* Etape 1 : créer une socket */   
+  int ds = socket(PF_INET, SOCK_STREAM, 0);
+
+  /* /!\ : Il est indispensable de tester les valeurs de retour de
+     toutes les fonctions et agir en fonction des valeurs
+     possibles. Voici un exemple */
+  if (ds == -1){
+    perror("Client : pb creation socket :");
+    exit(1); // je choisis ici d'arrêter le programme car le reste
+     // dépendent de la réussite de la création de la socket.
+  }
+
+  struct sockaddr_in server_addr;
+  server_addr.sin_family = PF_INET;
+  server_addr.sin_port = htons(atoi(argv[2]));
+  server_addr.sin_addr.s_addr = inet_addr(argv[1]);
+
+  socklen_t server_len = sizeof(server_addr);
+
+  if(connect(ds, (struct sockaddr*)&server_addr, server_len) < 0){
+    perror("erreur de connect");
+  }
+
+  char *msg = "sfdghqsfghjfgfhjfguyqksqbyqyebrcycbtrqejyrctbquyetbrfyqketryer";
+
+  printf("j'envoie le message : %s , de taille %ld\n", msg, strlen(msg));
+  int resSend1 = send(ds, msg, strlen(msg), 0);
+  printf("%d octets écrits\n", resSend1); // un send reste local TANT QUE PAS DE ACCEPT DE SERVEUR
+  if(resSend1 < 0){
+    perror("echec send");
+  }
+  printf("j'envoie le message : %s , de taille %ld\n", msg, strlen(msg));
+  int resSend2 = send(ds, msg, strlen(msg), 0);
+  printf("%d octets écrits\n", resSend2); // un send reste local TANT QUE PAS DE ACCEPT DE SERVEUR
+  if(resSend2 < 0){
+    perror("echec send");
+  }
+
+  int total = resSend1 + resSend2;
+  printf("%d octets envoyés au total\n",total);
+
+  int recu;
+  recv(ds, &recu, sizeof(int), 0);
+  printf("Message reçu du serveur : %d\n", recu);
+  
+  /* Etape 6 : fermer la socket (lorsqu'elle n'est plus utilisée)*/
+  
+  printf("Client : je termine\n");
+  close(ds);
+  return 0;
+}
---
+++ HAI604I - Programmation multi-tâches/TP2/server2chaines.c
@@ -0,0 +1,71 @@
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+
+  if (argc != 2) {
+    printf("utilisation : %s port_serveur\n", argv[0]);
+    exit(1);
+  }
+
+  int ds = socket(PF_INET, SOCK_STREAM, 0);
+  if (ds == -1) {
+    perror("Serveur : pb creation socket :");
+    exit(1);
+  }
+  printf("Serveur : création de la socket réussie \n");
+
+  struct sockaddr_in serverAddr;
+  serverAddr.sin_family = AF_INET;
+  serverAddr.sin_port = htons(atoi(argv[1]));
+  serverAddr.sin_addr.s_addr = INADDR_ANY;
+
+  socklen_t tailleStructServ = sizeof(serverAddr);
+  if (bind(ds, (struct sockaddr *)&serverAddr, tailleStructServ) < 0) {
+    perror("Serveur : erreur lors du bind :");
+    close(ds);
+    exit(1);
+  }
+  printf("Serveur : bind réussi\n");
+
+  if(listen(ds, 10) < 0){
+    perror("erreur de listen");
+  }
+
+  struct sockaddr_in clientAddr;
+  socklen_t tailleStructClient = sizeof(clientAddr);
+
+  int dsClient = accept(ds, (struct sockaddr *)&clientAddr, &tailleStructClient);
+
+  if(dsClient < 0){
+    perror("erreur de accept");
+  }
+
+  char msgCli[4000] = {0};
+  
+  int rcv = recv(dsClient, msgCli, sizeof(msgCli), 0);
+  if(rcv < 0){
+    perror("erreur recv");
+  }
+
+  msgCli[rcv] = '\0';
+  printf("Serveur : reçu '%s' (%d octets) de %s:%d\n", msgCli, rcv, inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port));
+
+  int resp = strlen(msgCli);
+  if (send(dsClient, &resp, sizeof(int), 0) < 0) {
+    perror("Serveur : erreur envoi réponse :");
+  } else {
+    printf("Serveur : réponse envoyée : %d octets\n", resp);
+  }
+
+  close(ds);
+  printf("Serveur : je termine\n");
+  return 0;
+}
