5487b767775c5a10a431e69159cbaf64a77a4221
thibaud.paulin01@etu.umontpellier.fr <thibaud.paulin@gmail.com>
2025-02-17 12:48:59 +0100

    étapz 1 du multi thread finie

---
+++ HAI604I - Programmation multi-tâches/TP4/exo1.c
@@ -0,0 +1,55 @@
+#include <string.h>
+#include <stdio.h>//perror
+#include <sys/types.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include "calcul.h"
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+struct paramsFonctionThread {
+  int idThread;
+  int *a;
+};
+
+void * fonctionThread (void * params){
+  struct paramsFonctionThread * args = (struct paramsFonctionThread *) params;
+  printf("Le thread %d commence\n", args->idThread);
+  calcul(1);
+
+  pthread_mutex_lock(&mutex);
+  *(args->a) += 1;
+  printf("a = %d\n", *(args->a));
+  pthread_mutex_unlock(&mutex);
+
+  printf("Le thread %d termine\n", args->idThread);
+}
+
+int main(int argc, char * argv[]){
+
+  if (argc < 2 ){
+    printf("utilisation: %s  nombre_threads  \n", argv[0]);
+    return 1;
+  }
+  
+  pthread_t threads[atoi(argv[1])];
+  struct paramsFonctionThread structuresParams[atoi(argv[1])];
+  int var = 12;
+
+  for (int i = 0; i < atoi(argv[1]); i++){
+
+    structuresParams[i].idThread = i;
+    structuresParams[i].a = &var;
+
+    if (pthread_create(&threads[i], NULL, fonctionThread, &structuresParams[i]) != 0){
+      perror("erreur creation thread");
+      exit(1);
+    }
+  }
+
+  printf("saisir un caractère \n");
+  fgetc(stdin);
+
+  return 0;
+}
